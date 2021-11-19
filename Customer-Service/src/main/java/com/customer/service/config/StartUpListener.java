0587d23e37ef8df33c32ae0032c284d9239c7306
emrekaradag <aliemrekaradag1@gmail.com>
2021-11-20 02:16:48 +0300

    Listener added

---
+++ Customer-Service/src/main/java/com/customer/service/config/StartUpListener.java
@@ -0,0 +1,23 @@
+package com.customer.service.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class StartUpListener {
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Override
+    public final void onApplicationEvent(ContextRefreshedEvent event) {
+        log.info("On Application Event is OK");
+        cacheManager.getCacheNames().parallelStream().forEach(n -> {
+            log.info(n);
+        });
+    }
+}
